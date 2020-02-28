e23e60f3b584b871708c75953a1ec215bd66f212
Alex Payne <alexander.payne@nottingham.ac.uk>
2020-02-28 15:07:33 +0000

    Refactor validator to use load_config_toml

--- ru/validate.py
+++ ru/validate.py
@@ -1,57 +1,26 @@
-import argparse
-import json
 import sys
-from pathlib import Path
-import re
+import argparse
 
-import toml
-import jsonschema
+from ru.utils import load_config_toml
 
 
-def any_full_match(patterns, string):
-    return any((re.fullmatch(p, string) for p in patterns))
+def except_hook(type, value, traceback):
+    print(value, file=sys.stderr)
 
 
 def main():
-    parser = argparse.ArgumentParser("RU3 TOML Validator ({})".format(__file__))
-    parser.add_argument("toml", help="TOML file")
-    parser.add_argument("schema", help="JSON schema")
-    parser.add_argument("-t", help="Validate target file", action="store_true")
+    # Catch exceptions and only print error line
+    sys.excepthook = except_hook
+
+    # Parse single positional argument
+    parser = argparse.ArgumentParser(
+        "Read Until TOML Validator ({})".format(__file__)
+    )
+    parser.add_argument("toml", help="TOML file to validate")
     args = parser.parse_args()
 
-    d = toml.load(args.toml)
-    with open(args.schema) as fh:
-        s = json.load(fh)
-
-    try:
-        jsonschema.validate(d, s)
-    except jsonschema.exceptions.ValidationError as err:
-        print(
-            "😾 this TOML file is not valid and may not work with Read Until:",
-            err,
-            sep="\n\n",
-        )
-        sys.exit(1)
-
-    patterns = [
-        v
-        for p in s["definitions"]["conditions"]["patternProperties"]["^[0-9]+$"][
-            "properties"
-        ]["targets"]["items"]["oneOf"]
-        for k, v in p.items()
-    ]
-
-    if args.t:
-        for k, v in d["conditions"].items():
-            if not isinstance(v, dict):
-                continue
-
-            if isinstance(v["targets"], str) and Path(v["targets"]).is_file():
-                with open(v["targets"], "r") as fh:
-                    for i, line in enumerate(fh, start=1):
-                        line = line.strip()
-                        if not any_full_match(patterns, line):
-                            print("{}:{} is invalid ({})".format(v["targets"], i, line))
+    # Run load config to validate
+    load_config_toml(args.toml, validate=True)
 
 
 if __name__ == "__main__":
