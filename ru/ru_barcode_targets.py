c2beb7e3af1147354d07c9cd1b4f6d8b86d9910b
Alex Payne <alexander.payne@nottingham.ac.uk>
2022-03-29 14:59:36 +0100

    Fix insecure port use in barcode targets

--- ru/ru_barcode_targets.py
+++ ru/ru_barcode_targets.py
@@ -526,7 +526,7 @@ def run(parser, args):
 
     read_until_client = RUClient(
         mk_host=position.host,
-        mk_port=position.description.rpc_ports.insecure,
+        mk_port=position.description.rpc_ports.secure,
         filter_strands=True,
         cache_type=AccumulatingCache,
     )
