ed0c822b4236a07be937655e6d369cd3db3697d4
Alex Payne <alexander.payne@nottingham.ac.uk>
2023-05-05 12:26:12 +0100

    Add unblock all script
    Add an unblock all script that uses a TOML file and the central
    `targets.py` entry point. This has been tested using playback on a mac
    but nowhere else currently. The TOML file is created as a temporary file
    that is then passed through using the command line arguments.
    
    This adds the `no_op` plugins. This is particularly useful for alignment
    not so much for basecalling. It allows a hypothetical user to place all
    of their logic in their basecaller module and exclude the separate
    alignment step all together.

--- src/readfish/entry_points/unblock_all.py
+++ src/readfish/entry_points/unblock_all.py
@@ -1,154 +1,42 @@
-"""unblock_all.py
+from tempfile import NamedTemporaryFile
 
-ReadUntil implementation that will only unblock reads. This should result in
-a read length histogram that has very short peaks (~280-580bp) as these are the
-smallest chunks that we can acquire. If you are not seeing these peaks, the
-`split_reads_after_seconds` parameter in the configuration file may need to be
-edited to 0.2-0.4.
-"""
-# Core imports
-import logging
-import sys
-import time
-from timeit import default_timer as timer
-
-# Read Until imports
 from readfish._cli_args import BASE_ARGS
-from readfish._utils import get_device, send_message, Severity
-from readfish._client import RUClient
-from readfish._loggers import print_args
-from read_until.read_cache import AccumulatingCache
-
+from readfish.entry_points import targets
 
 _help = "Unblock all reads"
-_cli = BASE_ARGS
+_cli = BASE_ARGS + (
+    (
+        "--debug-log",
+        dict(
+            help="Debug log, write a TSV for all records",
+            default=None,
+            dest="chunk_log",
+        ),
+    ),
+)
+
+TOML = """\
+[caller_settings.no_op]
+[mapper_settings.no_op]
+[[regions]]
+name = "unblock all"
+min_chunks = 1
+max_chunks = 2
+targets = []
+single_on = "unblock"
+single_off = "unblock"
+multi_on = "unblock"
+multi_off = "unblock"
+no_seq = "unblock"
+no_map = "unblock"
+above_max_chunks = "unblock"
+below_min_chunks = "unblock"
+"""
 
 
-def simple_analysis(
-    client, duration, batch_size=512, throttle=0.4, unblock_duration=0.1
-):
-    """Analysis function
-
-    Parameters
-    ----------
-    client : read_until_api.ReadUntilClient
-        An instance of the ReadUntilClient object
-    duration : int
-        Time to run for, in seconds
-    batch_size : int
-        The number of reads to be retrieved from the ReadUntilClient at a time
-    throttle : int or float
-        The number of seconds interval between requests to the ReadUntilClient
-    unblock_duration : int or float
-        Time, in seconds, to apply unblock voltage
-
-    Returns
-    -------
-    None
-    """
-    run_duration = time.time() + duration
-    logger = logging.getLogger(__name__)
-    send_message(
-        client.connection,
-        "ReadFish sending Unblock All Messages. All reads will be prematurely truncated. This will affect a live sequencing run.",
-        Severity.WARN,
-    )
-    while client.is_running and time.time() < run_duration:
-        r = 0
-        t0 = timer()
-        unblock_batch_action_list = []
-        stop_receiving_action_list = []
-        for r, (channel, read) in enumerate(
-            client.get_read_chunks(
-                batch_size=batch_size,
-                last=True,
-            ),
-            start=1,
-        ):
-            # Adding the channel and read.number to a list for a later batched unblock.
-            unblock_batch_action_list.append((channel, read.number, read.id))
-            stop_receiving_action_list.append((channel, read.number))
-
-        if len(unblock_batch_action_list) > 0:
-            client.unblock_read_batch(
-                unblock_batch_action_list, duration=unblock_duration
-            )
-            client.stop_receiving_batch(stop_receiving_action_list)
-
-        t1 = timer()
-        if r:
-            logger.info("Took {:.6f} for {} reads".format(t1 - t0, r))
-        # limit the rate at which we make requests
-        if t0 + throttle > t1:
-            time.sleep(throttle + t0 - t1)
-    else:
-        send_message(
-            client.connection, "ReadFish Unblock All Disconnected.", Severity.WARN
-        )
-        logger.info("Finished analysis of reads as client stopped.")
-
-
-def main():
-    sys.exit(
-        "This entry point is deprecated, please use 'readfish unblock-all' instead"
-    )
-
-
-def run(parser, args):
-    # TODO: Move logging config to separate configuration file
-    # TODO: use setup_logger here instead?
-    # set up logging to file for DEBUG messages and above
-    logging.basicConfig(
-        level=logging.DEBUG,
-        # TODO: args.log_format
-        format="%(levelname)s %(asctime)s %(name)s %(message)s",
-        filename=args.log_file,
-        filemode="w",
-    )
-
-    # define a Handler that writes INFO messages or higher to the sys.stderr
-    console = logging.StreamHandler()
-    console.setLevel(logging.INFO)
-
-    # set a format which is simpler for console use
-    formatter = logging.Formatter(args.log_format)
-    console.setFormatter(formatter)
-
-    # add the handler to the root logger
-    logging.getLogger("").addHandler(console)
-
-    # Start by logging sys.argv and the parameters used
-    logger = logging.getLogger("Manager")
-    logger.info(" ".join(sys.argv))
-    print_args(args, printer=logger.info)
-
-    position = get_device(args.device, host=args.host, port=args.port)
-
-    read_until_client = RUClient(
-        mk_host=position.host,
-        mk_port=position.description.rpc_ports.secure,
-        filter_strands=True,
-        cache_type=AccumulatingCache,
-    )
-
-    read_until_client.run(
-        first_channel=args.channels[0],
-        last_channel=args.channels[-1],
-    )
-
-    try:
-        simple_analysis(
-            client=read_until_client,
-            duration=args.run_time,
-            batch_size=args.batch_size,
-            throttle=args.throttle,
-            unblock_duration=args.unblock_duration,
-        )
-    except KeyboardInterrupt:
-        pass
-    finally:
-        read_until_client.reset()
-
-
-if __name__ == "__main__":
-    main()
+def run(parser, args, extras):
+    with NamedTemporaryFile("wt") as fh:
+        args.toml = fh.name
+        fh.write(TOML)
+        fh.flush()
+        targets.run(parser, args, extras)
