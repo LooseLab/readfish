16aa470a2eb23c539ef04609bd6c18a3bf4a18eb
Adoni5 <roryjmunro1@gmail.com>
2023-10-17 11:54:49 +0100

    Add docstring and doctests in readfish Add allowing ellipses in doctests to match lambdas with memory addresses

--- src/readfish/plugins/_filter.py
+++ src/readfish/plugins/_filter.py
@@ -1,5 +1,36 @@
-"""_filter.py
 """
+_filter.py
+
+Provides functionality to filter alignments in the PAF format using a mini-language.
+Users can define complex filtering conditions using this language, and the module
+parses these conditions to filter PAF records accordingly.
+
+Key Features:
+- Tokenization of user-defined filter rules.
+- Conversion of tokenized rules into callable filter functions.
+- Support for various comparison operators like '<', '>', '==', etc.
+- Ability to chain multiple filter rules together for compound filtering.
+
+Example Usage:
+    filter_rules = [
+        "is_primary",
+        "mapq > 40",
+        "strand == -1",
+    ]
+    filters = Filters(filter_rules)
+    for paf_record in paf_records:
+        if filters(paf_record):
+            process(paf_record)
+
+Notes:
+- This module is designed for extensibility, allowing for potential future enhancements
+  such as adding logical operators, grouping conditions, and more.
+- Users are encouraged to ensure their filter rules are correctly formatted to avoid
+  potential parsing errors.
+"""
+
+# ... rest of the code ...
+
 from __future__ import annotations
 import operator
 import re
@@ -32,6 +63,33 @@ _TOKEN_REGEX = re.compile("|".join(_SPEC))
 
 
 def tokenize(rule: str) -> dict[str, Any]:
+    """
+    Tokenize the given filtering rule string into its constituent parts.
+
+    :param rule: The filtering rule string to tokenize.
+    :return: A dictionary containing the tokenized elements of the rule.
+
+    :Example:
+
+    .. doctest::
+
+        >>> tokenize("is_primary")
+        {'ATTR': 'is_primary'}
+        >>> tokenize("mapq > 40")
+        {'ATTR': 'mapq', 'OP': <built-in function gt>, 'VALUE': 40.0}
+        >>> tokenize("strand == -1")
+        {'ATTR': 'strand', 'OP': <built-in function eq>, 'VALUE': -1.0}
+        >>> tokenize('"filename" in attr') # doctest: +ELLIPSIS
+        {'VALUE': 'filename', 'OP': <function <lambda> at ...>, 'ATTR': 'attr'}
+
+    .. note::
+
+        - The function can handle different types of rules, including those with
+          comparison operators, attribute checks, and checks for membership (using "in").
+        - The function recognizes and tokenizes attributes, comparison operators,
+          numbers, and quoted strings.
+    """
+
     def _parse(rule: str):
         for match in _TOKEN_REGEX.finditer(rule):
             kind = match.lastgroup
@@ -53,7 +111,39 @@ def tokenize(rule: str) -> dict[str, Any]:
 
 
 class Filter:
-    def __init__(self, rule):
+    """
+    Represents a single filter condition parsed from a rule string.
+
+    This class tokenizes a rule, interprets its meaning, and provides a callable
+    interface to evaluate the rule against given objects.
+
+    :param rule: The filtering rule string to parse and interpret.
+
+    :Example:
+
+    .. doctest::
+
+        >>> filter1 = Filter("is_primary")
+        >>> filter1({"is_primary": True, "mapq": 20, "strand": -1})
+        True
+        >>> filter2 = Filter("mapq > 40")
+        >>> filter2({"is_primary": True, "mapq": 50, "strand": -1})
+        True
+        >>> filter2({"is_primary": True, "mapq": 30, "strand": -1})
+        False
+        >>> filter3 = Filter("strand == -1")
+        >>> filter3({"is_primary": True, "mapq": 50, "strand": -1})
+        True
+
+    .. note::
+
+        - The `Filter` class can handle different types of rules, including attribute checks,
+          comparison operations, and membership checks.
+        - The class provides a callable interface, allowing it to be used as a function
+          to evaluate objects based on the rule.
+    """
+
+    def __init__(self, rule: str):
         self.rule = rule
         self._parts = tokenize(self.rule)
         self.get_fn = partial(nested_get, key=self._parts["ATTR"])
@@ -76,6 +166,40 @@ class Filter:
 
 
 class Filters:
+    """
+    Represents a collection of filter conditions parsed from a list of rule strings.
+
+    This class aggregates multiple `Filter` objects and provides a unified interface
+    to evaluate a set of filter conditions against given objects. It evaluates whether
+    an object satisfies *all* the provided conditions.
+
+    :param rules: A list of filtering rule strings to parse and interpret.
+    :type rules: List[str]
+
+    :Example:
+
+    .. doctest::
+
+        >>> rules = ["is_primary", "mapq > 40", "strand == -1"]
+        >>> filters = Filters(rules)
+        >>> obj1 = {"is_primary": True, "mapq": 50, "strand": -1}
+        >>> obj2 = {"is_primary": False, "mapq": 50, "strand": -1}
+        >>> obj3 = {"is_primary": True, "mapq": 30, "strand": -1}
+        >>> filters(obj1)
+        True
+        >>> filters(obj2)
+        False
+        >>> filters(obj3)
+        False
+
+    .. note::
+
+        - The `Filters` class aggregates multiple individual `Filter` objects.
+        - The class provides a callable interface, allowing it to be used as a function
+          to evaluate whether objects satisfy all the filtering conditions.
+        - If any rule is not satisfied, the `Filters` callable returns `False`.
+    """
+
     def __init__(self, rules):
         self.rules = rules
         self.funcs = [Filter(rule) for rule in self.rules]
