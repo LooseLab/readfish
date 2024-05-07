dc6644cef7bf9acfcba3889fa5b7490b23c7240a
Adoni5 <roryjmunro1@gmail.com>
2024-05-07 11:28:55 +0100

    Correct docstring, add doctests

--- src/readfish/_compatibility.py
+++ src/readfish/_compatibility.py
@@ -1,10 +1,13 @@
 """_compatibility.py
 
-Contains utilities for checking readfish compatibility with various versions of MinKNOW and Dorado.
-
-Throws a warning for the last supported version of MinKNOW for this version of readfish
+Contains utilities for checking readfish compatibility with various versions of MinKNOW.
 
 Checks ranges of `readfish` against the `MinKNOW` version
+
+Attributes:
+    LATEST_TESTED (str): The latest tested version of MinKNOW.
+    MINKNOW_COMPATIBILITY_RANGE (tuple): The compatibility range of MinKNOW versions for this version of readfish.
+    DIRECTION (Enum): An enumeration representing upgrade, downgrade, or no change directions.
 
 """
 
@@ -16,7 +19,7 @@ from enum import Enum
 LATEST_TESTED = "5.9.7"
 
 # The versions of MinKNOW which this version of readfish can connect to
-# Format - (lowest minknow, version of minknow not supported as an upper bound)
+# Format - (lowest minknow version, highest version of minknow supported as an upper bound)
 MINKNOW_COMPATIBILITY_RANGE = (
     Version("5.0.0"),
     Version(LATEST_TESTED),
@@ -50,13 +53,25 @@ def _get_minknow_version(host: str = "127.0.0.1", port: int = None) -> Version:
 
 def check_compatibility(
     comparator: Version,
-    version_range: tuple[Version, Version],
-) -> bool:
+    version_range: tuple[Version, Version] = MINKNOW_COMPATIBILITY_RANGE,
+) -> tuple[bool, DIRECTION]:
     """
-    Check the compatibility of a given software version, between a given range.
+    Check the compatibility of a given software version, between a given range,
+    inclusive of the right edge.
 
     :param comparator: Version of the provided software, for example MinKNOW 5.9.7
     :param version_ranges: A tuple of lowest supported version, highest supported version
+
+    :return: A tuple containing a boolean indicating compatibility and a upgrade direction enum.
+
+    Examples:
+    >>> from packaging.version import Version
+    >>> check_compatibility(Version("5.9.5"), (Version("5.0.0"), Version("5.9.7")))
+    (True, <DIRECTION.JUST_RIGHT: 'do nothing'>)
+    >>> check_compatibility(Version("5.9.8"), (Version("5.0.0"), Version("5.9.7")))
+    (False, <DIRECTION.UPGRADE: 'upgrade'>)
+    >>> check_compatibility(Version("4.9.0"), (Version("5.0.0"), Version("5.9.7")))
+    (False, <DIRECTION.DOWNGRADE: 'downgrade'>)
     """
     (
         lowest_supported_version,
