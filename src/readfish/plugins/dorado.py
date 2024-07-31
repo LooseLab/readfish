891393f497bb8694591a97323a9504e933c22f18
Adoni5 <roryjmunro1@gmail.com>
2024-07-31 17:00:31 +0100

    Update test for conencted base caller version to work

--- src/readfish/plugins/dorado.py
+++ src/readfish/plugins/dorado.py
@@ -67,16 +67,19 @@ class Caller(CallerABC):
         self.supported_barcode_kits = None
         self.supported_basecall_models = None
         self.run_information = run_information
+
         if self.run_information:
-            self.guppy_version = (
-                self.run_information.software_versions.guppy_connected_version
+            self.basecaller_version = (
+                self.run_information.software_versions.basecaller_connected_version
             )
 
-            if parse_version(self.guppy_version) >= parse_version("7.3.9"):
-                logging.info(f"Connected to caller version {self.guppy_version}.")
+            if parse_version(self.basecaller_version) >= parse_version("7.4.12"):
+                logging.info(f"Connected to caller version {self.basecaller_version}.")
             else:
                 logging.info(
-                    f"Trying to use minKNOW with a caller version {self.guppy_version}. If this is causing readfish to crash, try using a version of Dorado >= 7.3.9. You should also check for any updates available to readfish."
+                    f"Trying to use minKNOW with a caller version {self.basecaller_version}."
+                    " If this is causing readfish to crash, try using a version of Dorado >= 7.3.9."
+                    " You should also check for any updates available to readfish."
                 )
 
         # Set our own priority
