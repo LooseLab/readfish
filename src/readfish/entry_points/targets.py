b4a1e49dc120c0a912994c5d99eed7d36f45e480
Adoni5 <roryjmunro1@gmail.com>
2025-04-28 21:21:13 +0100

    Remove unused read classifications for minknow 6.4.X

--- src/readfish/entry_points/targets.py
+++ src/readfish/entry_points/targets.py
@@ -623,8 +623,6 @@ If there isn't a newer version of readfish and readfish is failing, please open 
         timeout=args.wait_for_ready,
         prefilter_classes={
             "strand",
-            "strand2",
-            "short_strand",
             "adapter",
             "unknown_positive",
         },
@@ -654,8 +652,6 @@ If there isn't a newer version of readfish and readfish is failing, please open 
         max_unblock_read_length_seconds=args.max_unblock_read_length_seconds,
         accepted_first_chunk_classifications=[
             "strand",
-            "strand2",
-            "short_strand",
             "adapter",
             "unknown_positive",
         ],
